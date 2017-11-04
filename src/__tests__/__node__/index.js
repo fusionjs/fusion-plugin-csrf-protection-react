a81eafab64e9f7e100271fa7acd56bcfbe5b5c2d
Giancarlo Anemone <ganemone@gmail.com>
2017-11-04 11:25:38 -0700

    Upgrade versions of fusion-react and fusion-plugin-csrf-protection (#7)

--- src/__tests__/__node__/index.js
+++ src/__tests__/__node__/index.js
@@ -1,18 +1,28 @@
+import React from 'react';
 import test from 'tape-cup';
 import Plugin from '../../plugin';
 import withFetch from '../../hoc';
+import ShallowRenderer from 'react-test-renderer/shallow';
 
 test('plugin', t => {
   t.equals(typeof Plugin, 'function');
   const Session = {get() {}, set() {}};
   const CsrfProtection = Plugin({Session});
-  t.equals(typeof CsrfProtection.Service.ignore, 'function');
+  t.equals(typeof CsrfProtection.of().ignore, 'function');
   t.end();
 });
 test('hoc', t => {
   function Test() {}
-  const hoc = withFetch(Test);
+  const Connected = withFetch(Test);
   t.equals(typeof withFetch, 'function');
-  t.equals(hoc.displayName, 'WithCsrfProtection(Test)');
+  t.equals(Connected.displayName, 'WithCsrfProtection(Test)');
+  const renderer = new ShallowRenderer();
+  renderer.render(React.createElement(Connected), {
+    csrfProtection: {
+      fetch: () => {},
+    },
+  });
+  const rendered = renderer.getRenderOutput();
+  t.equal(typeof rendered.props.fetch, 'function');
   t.end();
 });
